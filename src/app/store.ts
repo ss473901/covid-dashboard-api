10c966dbe2a842e434a28705bde58b4af866abbd
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-06-09 19:52:32 +0900

    first commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import covidReducer from "../features/covid/covidSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    covid: covidReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
