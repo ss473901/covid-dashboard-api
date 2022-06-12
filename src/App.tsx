e8e2bd70b15deac20f17302f0958a02b863da5cd
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-06-12 17:08:10 +0900

    Add DaschBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import DashBoard from "./features/covid/DashBoard/DashBoard";
 import PieChart from "./features/covid/PieChart/PieChart";
 
 function App() {
   return (
     <>
-      <PieChart />
+      <DashBoard />
     </>
   );
 }
--- src/features/covid/DashBoard/DashBoard.tsx
+++ src/features/covid/DashBoard/DashBoard.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import styles from "./DashBoard.module.css";
 
-const DashBoard = () => {
-  return <div>DashBoard</div>;
+import { useAppDispatch } from "../../../app/hooks";
+import { fetchAsyncGetDaily } from "../covidSlice";
+
+import PieChart from "../PieChart/PieChart";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+  content: {
+    marginTop: 85,
+  },
+}));
+
+const DashBoard: React.FC = () => {
+  const classes = useStyles();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAsyncGetDaily("japan"));
+  }, [dispatch]);
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Covid 19 Live DashBoard
+          </Typography>
+          <PieChart />
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default DashBoard;
